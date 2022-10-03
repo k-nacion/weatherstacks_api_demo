be0c23383b7f31a259138774880f4e0254709747
Kenn <kentot51810@gmail.com>
2022-10-02 23:48:42 -0500

    [ADD] getCurrentWeatherUseCase added

---
+++ lib/features/weather/domain/use_cases/get_current_weather_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:weatherstacks_api_demo/error/failures.dart';
+import 'package:weatherstacks_api_demo/features/weather/domain/entities/weather.dart';
+import 'package:weatherstacks_api_demo/features/weather/domain/repositories/weather_repository.dart';
+
+class GetCurrentWeatherUseCase {
+  final WeatherRepository _repository;
+
+  const GetCurrentWeatherUseCase({
+    required WeatherRepository repository,
+  }) : _repository = repository;
+
+  Future<Either<Failure, Weather>> call() async => await _repository.getCurrentWeather();
+}
