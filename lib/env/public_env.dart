abd3bc0560debb24e5af4b7c1655efc7ab9f1133
k-nacion <kentot51810@gmail.com>
2022-10-03 22:44:25 +0800

    [MOD] include generated file for public environment

--- lib/env/public_env.dart
+++ lib/env/public_env.dart
@@ -1,9 +1,10 @@
 import 'package:envied/envied.dart';
+import 'package:weatherstacks_api_demo/env/private_env.dart';
 
 part 'public_env.g.dart';
 
 @Envied(requireEnvFile: true, path: '.env')
 abstract class PublicEnv {
   @EnviedField(varName: 'WEATHER_STACK_BASE_URL')
-  static const weatherStackBaseUrl = _PublicEnv.weatherStackBaseUrl;
+  static final weatherStackBaseUrl = _PublicEnv.weatherStackBaseUrl;
 }
---
+++ lib/env/public_env.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'public_env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _PublicEnv {
+  static final weatherStackBaseUrl = 'http://api.weatherstack.com/current?access_key=${PrivateEnv.weatherStackApiKey}&query=';
+}
