8977bfe236e87508ba8349d31cd0ba6d8ce5268c
Kenn <kentot51810@gmail.com>
2022-10-02 23:32:42 -0500

    [ADD] weather entity

--- lib/features/weather/domain/entities/weather.dart
+++ lib/features/weather/domain/entities/weather.dart
@@ -1,1 +1,49 @@
+import 'package:equatable/equatable.dart';
 
+class Weather extends Equatable {
+  final int temperature;
+  final int weatherCode;
+  final List<String> weatherIcons;
+  final List<String> weatherDescriptions;
+  final int? windSpeed;
+  final int? windDegree;
+  final String? windDirection;
+  final int? pressure;
+  final int? precip;
+  final int? humidity;
+  final int? cloudCover;
+
+  const Weather({
+    required this.temperature,
+    required this.weatherCode,
+    required this.weatherIcons,
+    required this.weatherDescriptions,
+    this.windSpeed,
+    this.windDegree,
+    this.windDirection,
+    this.pressure,
+    this.precip,
+    this.humidity,
+    this.cloudCover,
+  });
+
+  @override
+  String toString() {
+    return 'Weather{temperature: $temperature, weatherCode: $weatherCode, weatherIcons: $weatherIcons, weatherDescriptions: $weatherDescriptions, windSpeed: $windSpeed, windDegree: $windDegree, windDirection: $windDirection, pressure: $pressure, precip: $precip, humidity: $humidity, cloudCover: $cloudCover}';
+  }
+
+  @override
+  List<Object?> get props => [
+        temperature,
+        weatherCode,
+        weatherIcons,
+        weatherDescriptions,
+        windSpeed,
+        windDegree,
+        windDirection,
+        pressure,
+        precip,
+        humidity,
+        cloudCover,
+      ];
+}
