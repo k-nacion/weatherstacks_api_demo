4d91d5d0755c7589df75fcdc8b1053229fa622ff
k-nacion <kentot51810@gmail.com>
2022-10-05 05:08:27 +0800

    [MOD] [DATA LAYER] partially implemented getCurrentWeather

--- lib/features/weather/data/repositories/weather_repository_impl.dart
+++ lib/features/weather/data/repositories/weather_repository_impl.dart
@@ -1,13 +1,29 @@
 import 'package:fpdart/src/either.dart';
+import 'package:weatherstacks_api_demo/core/network/network_info.dart';
 import 'package:weatherstacks_api_demo/error/failures.dart';
+import 'package:weatherstacks_api_demo/features/weather/data/data_source/weather_local_data_source.dart';
+import 'package:weatherstacks_api_demo/features/weather/data/data_source/weather_remote_data_source.dart';
 import 'package:weatherstacks_api_demo/features/weather/domain/entities/weather.dart';
 import 'package:weatherstacks_api_demo/features/weather/domain/repositories/weather_repository.dart';
 
-class WeatherRepositoryImpl implements WeatherRepository{
-  @override
-  Future<Either<Failure, Weather>> getCurrentWeather() {
-    // TODO: implement getCurrentWeather
-    throw UnimplementedError();
-  }
+class WeatherRepositoryImpl implements WeatherRepository {
+  final WeatherLocalDataSource _localDataSource;
+  final WeatherRemoteDataSource _remoteDataSource;
+  final NetworkInfo _networkInfo;
 
+  const WeatherRepositoryImpl({
+    required WeatherLocalDataSource localDataSource,
+    required WeatherRemoteDataSource remoteDataSource,
+    required NetworkInfo networkInfo,
+  })  : _localDataSource = localDataSource,
+        _remoteDataSource = remoteDataSource,
+        _networkInfo = networkInfo;
+
+  @override
+  Future<Either<Failure, Weather>> getCurrentWeather(String location) async {
+    _networkInfo.hasInternetConnection;
+    final currentWeather = await _remoteDataSource.getCurrentWeather(location);
+
+    return Right(currentWeather);
+  }
 }
