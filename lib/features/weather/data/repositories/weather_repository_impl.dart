9746b6eb7e6d9304349d5da91f203503430502b8
k-nacion <kentot51810@gmail.com>
2022-10-10 16:36:53 +0800

    [MOD] [DATA LAYER] Moved network_info.dart to Data Source

--- lib/features/weather/data/repositories/weather_repository_impl.dart
+++ lib/features/weather/data/repositories/weather_repository_impl.dart
@@ -1,10 +1,11 @@
 import 'package:fpdart/src/either.dart';
-import 'package:weatherstacks_api_demo/core/network/network_info.dart';
 import 'package:weatherstacks_api_demo/error/failures.dart';
 import 'package:weatherstacks_api_demo/features/weather/data/data_source/weather_local_data_source.dart';
 import 'package:weatherstacks_api_demo/features/weather/data/data_source/weather_remote_data_source.dart';
 import 'package:weatherstacks_api_demo/features/weather/domain/entities/weather.dart';
 import 'package:weatherstacks_api_demo/features/weather/domain/repositories/weather_repository.dart';
+
+import '../data_source/network_info.dart';
 
 class WeatherRepositoryImpl implements WeatherRepository {
   final WeatherLocalDataSource _localDataSource;
