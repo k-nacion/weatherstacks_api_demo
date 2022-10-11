8116a3f323feec1e819c1070cd0abe5384156ab2
k-nacion <kentot51810@gmail.com>
2022-10-11 11:44:40 +0800

    [MOD] [DATA LAYER] added network implementation

--- lib/features/weather/data/data_source/network_info.dart
+++ lib/features/weather/data/data_source/network_info.dart
@@ -1,3 +1,17 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkInfo {
   Future<bool> get hasInternet;
+}
+
+class NetworkInfoImpl implements NetworkInfo {
+  final InternetConnectionChecker _internetConnectionChecker;
+
+  const NetworkInfoImpl({
+    required InternetConnectionChecker internetConnectionChecker,
+  }) : _internetConnectionChecker = internetConnectionChecker;
+
+  @override
+  // TODO: implement hasInternet
+  Future<bool> get hasInternet async => await _internetConnectionChecker.hasConnection;
 }
