3337a73ba8401774828edfdb3c757ba8dcd386ff
k-nacion <kentot51810@gmail.com>
2022-10-11 11:45:46 +0800

    [ADD] [PRESENTATION LAYER] TODO: to add view that will represent data

---
+++ lib/features/weather/presentation/pages/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Weather Page'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
