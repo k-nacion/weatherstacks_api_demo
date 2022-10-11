bfed17c0736a419008e587ef2100551b63d5c6ad
k-nacion <kentot51810@gmail.com>
2022-10-11 10:34:06 +0800

    [MOD] [CORE LAYER] added Network as optional parameter for NoInternetFailure

--- lib/error/failures.dart
+++ lib/error/failures.dart
@@ -1,13 +1,21 @@
 import 'package:equatable/equatable.dart';
+import 'package:weatherstacks_api_demo/features/network/domain/entities/network.dart';
 
 abstract class Failure extends Equatable {
   const Failure();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-class NoInternetFailure extends Failure {}
+class NoInternetFailure extends Failure {
+  final Network? network;
+
+  const NoInternetFailure({this.network});
+
+  @override
+  List<Object?> get props => [network];
+}
 
 class CacheFailure extends Failure {}
 
