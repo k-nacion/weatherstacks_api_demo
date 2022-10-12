3a0e6e8f15d3bc01fd63ac20209a0bda3083785b
Kenn <kentot51810@gmail.com>
2022-10-12 08:57:38 -0500

    [GENERATED] no changes from codebase.

--- lib/env/private_env.dart
+++ lib/env/private_env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'private_env.g.dart';
 
-@Envied(path: '.env', requireEnvFile: true, obfuscate: true)
+@Envied(path: '.private.env', requireEnvFile: true, obfuscate: true)
 abstract class PrivateEnv {
   @EnviedField(varName: 'WEATHER_STACK_API_KEY')
   static final weatherStackApiKey = _PrivateEnv.weatherStackApiKey;
