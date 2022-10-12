9ee4f22995bbc6228eec6dcd5c706f405473e0e5
k-nacion <kentot51810@gmail.com>
2022-10-12 17:17:58 +0800

    [MOD] TODO: implement methods.

--- lib/features/weather/data/data_source/weather_local_data_source.dart
+++ lib/features/weather/data/data_source/weather_local_data_source.dart
@@ -1,6 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weatherstacks_api_demo/features/weather/data/model/weather_model.dart';
+
+const weatherDataKey = 'WEATHER_DATA_KEY';
 
 abstract class WeatherLocalDataSource {
   Future<void> cacheWeather(WeatherModel weather);
   Future<WeatherModel> getLastCachedWeatherData();
+}
+
+class WeatherLocalDataSourceImpl implements WeatherLocalDataSource {
+  final SharedPreferences _sharedPreferences;
+
+  const WeatherLocalDataSourceImpl({
+    required SharedPreferences sharedPreferences,
+  }) : _sharedPreferences = sharedPreferences;
+
+  @override
+  Future<void> cacheWeather(WeatherModel weather) async {
+    // TODO: implement cacheWeather
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<WeatherModel> getLastCachedWeatherData() async {
+    // TODO: implement getLastCachedWeatherData
+    throw UnimplementedError();
+  }
 }
