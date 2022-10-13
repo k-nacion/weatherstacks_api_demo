c68c6e5c1ece79430aeaa96a2aba4fafbf47d4bc
k-nacion <kentot51810@gmail.com>
2022-10-14 01:21:21 +0800

    [MOD] [WEATHER] [DATA LAYER] todo: add implementation

--- lib/features/weather/data/data_source/weather_remote_data_source.dart
+++ lib/features/weather/data/data_source/weather_remote_data_source.dart
@@ -1,5 +1,20 @@
+import 'package:http/http.dart';
 import 'package:weatherstacks_api_demo/features/weather/data/model/weather_model.dart';
 
 abstract class WeatherRemoteDataSource {
   Future<WeatherModel> getCurrentWeather(String location);
+}
+
+class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
+  final Client _httpClient;
+
+  const WeatherRemoteDataSourceImpl({
+    required Client httpClient,
+  }) : _httpClient = httpClient;
+
+  @override
+  Future<WeatherModel> getCurrentWeather(String location) {
+    // TODO: implement getCurrentWeather
+    throw UnimplementedError();
+  }
 }
