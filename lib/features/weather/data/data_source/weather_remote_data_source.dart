089b27c8c5310a3edc45ba28c3f112217494b300
k-nacion <kentot51810@gmail.com>
2022-10-15 02:07:57 +0800

    [MOD] [WEATHER] [DATA LAYER] implemented getCurrentWeather

--- lib/features/weather/data/data_source/weather_remote_data_source.dart
+++ lib/features/weather/data/data_source/weather_remote_data_source.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:http/http.dart';
+import 'package:weatherstacks_api_demo/env/env.dart';
+import 'package:weatherstacks_api_demo/env/private_env.dart';
+import 'package:weatherstacks_api_demo/error/exceptions.dart';
 import 'package:weatherstacks_api_demo/features/weather/data/model/weather_model.dart';
+
+const weatherUriHeader = {'content-type': 'application/json'};
 
 abstract class WeatherRemoteDataSource {
   Future<WeatherModel> getCurrentWeather(String location);
@@ -13,8 +20,24 @@ class WeatherRemoteDataSourceImpl implements WeatherRemoteDataSource {
   }) : _httpClient = httpClient;
 
   @override
-  Future<WeatherModel> getCurrentWeather(String location) {
-    // TODO: implement getCurrentWeather
-    throw UnimplementedError();
+  Future<WeatherModel> getCurrentWeather(String location) async {
+    final baseUrl = Env.WEATHER_STACK_BASE_URL;
+    final unencodedPath = Env.WEATHER_STACK_BASE_URL_UNENCODED_PATH;
+    final mapQuery = {
+      Env.WEATHER_STACK_BASE_URL_ACESS_KEY_QUERY: PrivateEnv.weatherStackApiKey,
+      Env.WEATHER_STACK_BASE_URL_QUERY_QUERY: location
+    };
+
+    final String response;
+    try {
+      response = await _httpClient.read(Uri.http(baseUrl, unencodedPath, mapQuery),
+          headers: weatherUriHeader);
+    } on ClientException {
+      throw ServerUnreachableException();
+    }
+
+    final weatherModel = WeatherModel.fromMap(jsonDecode(response)['current']);
+
+    return weatherModel;
   }
 }
