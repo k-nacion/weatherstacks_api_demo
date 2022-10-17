3cf134cd97e6a9b04d291ee199ba736af6c8fa06
Kenn <kentot51810@gmail.com>
2022-10-17 09:48:02 -0500

    [MOD] [NETWORK] [DATA LAYER] added implementation class

--- lib/features/network/data/data_sources/network_data_source.dart
+++ lib/features/network/data/data_sources/network_data_source.dart
@@ -1,3 +1,16 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
 abstract class NetworkDataSource {
   Future<bool> get hasInternetConnection;
+}
+
+class NetworkDataSourceImpl implements NetworkDataSource {
+  final InternetConnectionChecker _connectionChecker;
+
+  const NetworkDataSourceImpl({
+    required InternetConnectionChecker connectionChecker,
+  }) : _connectionChecker = connectionChecker;
+
+  @override
+  Future<bool> get hasInternetConnection => _connectionChecker.hasConnection;
 }
