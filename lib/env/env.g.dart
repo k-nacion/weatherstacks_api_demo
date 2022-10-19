61e6d2ab4e82f60f2982c47c28975df3b25e180e
Kenn <kentot51810@gmail.com>
2022-10-19 12:11:27 -0500

    [MOD] [ENV] added error messages as env

--- lib/env/env.g.dart
+++ lib/env/env.g.dart
@@ -9,9 +9,14 @@ part of 'env.dart';
 class _Env {
   static const WEATHER_STACK_BASE_URL = 'api.weatherstack.com';
   static const WEATHER_STACK_BASE_URL_UNENCODED_PATH = 'current';
-  static const WEATHER_STACK_BASE_URL_ACESS_KEY_QUERY = 'access_key';
+  static const WEATHER_STACK_BASE_URL_ACCESS_KEY_QUERY = 'access_key';
   static const WEATHER_STACK_BASE_URL_QUERY_QUERY = 'query';
   static const WEATHER_SHARED_PREF_KEY = 'WEATHER_SHARED_PREF_KEY';
   static const SUCCESS_JSON_RESPONSE = 'current_weather_success_response.json';
-  static const WEATHER_URI_HEADER = '{"Content-Type": "application/json"}';
+  static const WEATHER_CACHE_ERROR_MESSAGE =
+      'Oops! Cannot fetch weather. Try again later.';
+  static const WEATHER_NO_INTERNET_ERROR_MESSAGE =
+      'Your device is offline. Try again later.';
+  static const WEATHER_SERVER_UNREACHABLE_ERROR_MESSAGE =
+      'Server is offline right now. Try again later.';
 }
