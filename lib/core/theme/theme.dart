d12e71beea564ea8c7e72fc9f8a0487591558a2b
Kenn <kentot51810@gmail.com>
2022-10-24 09:09:49 -0500

    [ADD] added theme and font assets

---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+
+final lightTheme = FlexThemeData.light(
+    scheme: FlexScheme.espresso,
+    surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+    blendLevel: 20,
+    appBarOpacity: 0.95,
+    swapColors: true,
+    subThemesData: const FlexSubThemesData(
+      blendOnLevel: 20,
+      blendOnColors: false,
+    ),
+    useMaterial3ErrorColors: true,
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    fontFamily: 'General Sans');
