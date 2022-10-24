02a3e28efe8dd3502a0bc541469039f80f472a4c
Kenn <kentot51810@gmail.com>
2022-10-24 09:11:03 -0500

    [MOD] [WEATHER] [PRESENTATION] added BlocProvider

--- lib/features/weather/presentation/pages/weather_page.dart
+++ lib/features/weather/presentation/pages/weather_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weatherstacks_api_demo/features/weather/presentation/bloc/weather_bloc.dart';
+import 'package:weatherstacks_api_demo/features/weather/presentation/widgets/weather_body.dart';
+import 'package:weatherstacks_api_demo/features/weather/weather_di.dart';
 
 class WeatherPage extends StatelessWidget {
   const WeatherPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather Page'),
-        centerTitle: true,
-      ),
+    return BlocProvider<WeatherBloc>(
+      create: (context) => sl<WeatherBloc>(),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Weather Page'),
+            centerTitle: true,
+          ),
+          body: const WeatherBody()),
     );
   }
 }
